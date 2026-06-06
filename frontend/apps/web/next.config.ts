9be4fc9c61e12e9097009cc17e8d74995c7273b2
Thinh <thinh0704hcm@users.noreply.github.com>
2026-06-06 19:54:51 +0700

    feat: fix trailing slash leading to not branding page not rendering correctly

--- frontend/apps/web/next.config.ts
+++ frontend/apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   transpilePackages: ["@repo/api-client"],
   images: {
     unoptimized: true,
